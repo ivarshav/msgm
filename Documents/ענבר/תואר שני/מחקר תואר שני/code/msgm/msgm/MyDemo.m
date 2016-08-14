702abbecc08d51104e8e5b4f691e857f0e48e365
inbar varshavsky <inbvar@gmail.com>
2016-08-14 13:55:48 +0300

    get to normal exp

--- Documents/ענבר/תואר שני/מחקר תואר שני/code/msgm/msgm/MyDemo.m
+++ Documents/ענבר/תואר שני/מחקר תואר שני/code/msgm/msgm/MyDemo.m
@@ -9,7 +9,7 @@ function [eMS, tMS, eSS, tSS] = MyDemo()
 %   - COUPLING : coupling parameter, values >1 correspond to "harder" models
 %
     % experiment number
-    j = 27; %not real exp 
+    j = 45; %not real exp 
     
     % parameters
     GRID_SIZE = 70;
@@ -19,7 +19,7 @@ function [eMS, tMS, eSS, tSS] = MyDemo()
     
     % generate the adjacency relations for [GRID_SIZE x GRID_SIZE] grid
     sz = [GRID_SIZE, GRID_SIZE];
-    [ii, jj] = sparse_adj_matrix(sz, 1, 'inf');
+    [ii, jj] = sparse_adj_matrix(sz, 1, 1);
     sel = ii<jj;
     G.adj = [ii(sel), jj(sel)];
     
@@ -29,7 +29,7 @@ function [eMS, tMS, eSS, tSS] = MyDemo()
     param.optimization = 'QPBO';
     param.numSwapIterations = 1;
     param.bSoftInterpolation = false;
-    param.numVcycles = 15;
+    param.numVcycles = 10;
     
     % initialize output data variables
     eMS = zeros(N_REPETITIONS, param.numVcycles);
@@ -102,7 +102,7 @@ function [eMS, tMS, eSS, tSS] = MyDemo()
     descr = {strcat('numVcycles: ', num2str(param.numVcycles));
         strcat('numLabels: ', num2str(N_LABELS));
         strcat('Coupling: ', num2str(COUPLING));
-        strcat('Variable grouping: ', '8-connected');
+        strcat('Adjacency: ', '8-connected');
         };
     ax1 = axes('Position',[0 0 1 1],'Visible','off');
     axes(ax1) % sets ax1 to current axes
@@ -112,7 +112,7 @@ function [eMS, tMS, eSS, tSS] = MyDemo()
     print(fig, strcat('results/exp', num2str(j)), '-djpeg');
 
     xlswrite('exp.xls', ...
-        {j, GRID_SIZE, 'too long', N_REPETITIONS, param.optimization, param.numVcycles, param.bSoftInterpolation, '8-connected', N_LABELS, COUPLING, ReprVector(eMS(:, param.numVcycles)), ReprVector(tMS),... 
+        {j, GRID_SIZE, 'too long', N_REPETITIONS, param.optimization, param.numVcycles, param.bSoftInterpolation, 'Normal', N_LABELS, COUPLING, ReprVector(eMS(:, param.numVcycles)), ReprVector(tMS),... 
         ReprVector(eSS), ReprVector(tSS);}, 1, sprintf('A%d' ,(j+1)));
     
     
